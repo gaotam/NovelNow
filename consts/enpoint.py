9e7cc3d8f67116264fd91ba8373518c2f91ef68d
doanvu20122000 <doanvu20122000@gmail.com>
2025-04-24 08:16:24 +0700

    - Update endpoint truyenqq - update data 08:15 24/04/2025

--- consts/enpoint.py
+++ consts/enpoint.py
@@ -1,6 +1,6 @@
 from consts import ProviderName
 
 ENDPOINTS = {
-    ProviderName.TRUYENQQTO: "https://truyenqqto.com/truyen-tranh",
+    ProviderName.TRUYENQQTO: "https://truyenqqgo.com/truyen-tranh",
     ProviderName.METRUYENCHU: "https://backend.metruyencv.com/api/chapters",
 }
