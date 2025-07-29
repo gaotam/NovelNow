c645779a132220d33ef89040ce7f6297d057c263
doanvu20122000 <doanvu20122000@gmail.com>
2025-07-29 15:07:23 +0700

    update data and tests for story providers

--- tests/test_nettruyen_provider.py
+++ tests/test_nettruyen_provider.py
@@ -1,15 +1,16 @@
 import unittest
 
-from models.chapter import Status
+# from models.chapter import Status
+from models.story_info import StoryStatus
 from providers import NetTruyenProvider
-from providers.truyenqqto import TruyenQQTOProvider
+
 
 class TestNetTruyenProvider(unittest.TestCase):
     def test_fetches_latest_chapter_when_data_is_valid(self):
         provider = NetTruyenProvider(id="hoa-than-thanh-meo", last_chapter=5)
-        chapter_info = provider.get_latest_chapter()
+        chapter_info = provider.get_story_info()
         self.assertGreater(chapter_info.latest_chapter, 5)
-        self.assertIsNotNone(chapter_info.date)
+        self.assertIsNotNone(chapter_info.latest_chapter_date)
 
     # def test_completed_status_when_story_is_finished(self):
     #     provider = NetTruyenProvider(id="su-tro-lai-cua-phap-su-vi-dai-sau-4000-nam", last_chapter=212)
