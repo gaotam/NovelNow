8860bccc3a3e1fa2224745fcc83347621182a43c
Doan Vũ <doanvu20122000@gmail.com>
2026-05-23 12:35:53 +0700

    update: increment last chapters and update endpoint URLs

--- consts/enpoint.py
+++ consts/enpoint.py
@@ -2,7 +2,7 @@ from consts import ProviderName
 
 ENDPOINTS = {
     ProviderName.TRUYENQQTO: "https://truyenqqko.com/truyen-tranh",
-    ProviderName.NETTRUYEN: "https://nettruyenvio.com/truyen-tranh",
+    ProviderName.NETTRUYEN: "https://nettruyen.gg/truyen-tranh",
     ProviderName.METRUYENCHU: "https://backend.metruyencv.com/api/chapters",
-    ProviderName.GOCTRUYENTRANHVUI: "https://goctruyentranhvui19.com/truyen"
+    ProviderName.GOCTRUYENTRANHVUI: "https://goctruyentranhvui30.com/truyen"
 }
